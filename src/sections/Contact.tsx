aa6dd83582db4ad096912ab78ac920068de42ec2
Aman Deol <amandeolm@gmail.com>
2024-10-03 22:46:08 +0530

    done with about and footer section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -14,8 +14,8 @@ export const ContactSection = () => {
           
           <div className="flex flex-col md:flex-row gap-10 md:gap-20">
             <div>
-              <h2 className="font-serif text-bold text-2xl text-center md:text-3xl lg:text-4xl md:text-start">Let's Create something amazing together</h2>
-              <p className="mt-3 text-gray-950 font-medium text-center md:text-lg md:text-start">Ready to bring your next project to life? Lets connect and I can help you achieve your goals.</p>
+              <h2 className="font-serif text-bold text-2xl text-center md:text-3xl lg:text-4xl md:text-start">{`Let's`} Create something amazing together</h2>
+              <p className="mt-3 text-gray-950 font-medium text-center md:text-lg md:text-start">Ready to bring your next project to life? {`Let's`} connect and I can help you achieve your goals.</p>
             </div>
             <div className="flex justify-center items-center">
               <button className="rounded-xl bg-gray-950 py-4 px-6">
