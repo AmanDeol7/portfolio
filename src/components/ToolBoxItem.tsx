8066d3019a9ebf4e695ab82eac1137394dd72901
Aman Deol <amandeolm@gmail.com>
2024-10-25 18:30:27 +0530

    added animations

--- src/components/ToolBoxItem.tsx
+++ src/components/ToolBoxItem.tsx
@@ -17,7 +17,7 @@ export const ToolBoxItems = ({items, className, itemsWrapperClassName} : {
         <div className={twMerge("flex  [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] "+className)}>
         <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6 "+ itemsWrapperClassName)}>
           {[...new Array(3)].fill(0).map((_, index) => (
-            <Fragment>
+            <Fragment key={index}>
               {items.map((item,index) => {
           return (
             <div key={index} className="inline-flex items-center gap-4 py-2 px-3  outline outline-2 outline-white/10 rounded-lg ">
